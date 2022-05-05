3bad01f1a4e5384244810c36409c666c9a21f4e4
Adélaïde LOUIS <adelaide.louis78@hotmail.fr>
2022-05-05 09:43:29 +0200

    Modification de la gridView dans Default.java

--- projet/app/src/main/java/com/example/sokoban/Default.java
+++ projet/app/src/main/java/com/example/sokoban/Default.java
@@ -2,34 +2,29 @@ package com.example.sokoban;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.widget.AdapterView;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.GridView;
-import android.widget.Toast;
-import android.os.Bundle;
-
-import com.example.test.databinding.ActivityMainBinding;
 
 public class Default extends AppCompatActivity {
 
-    ActivityMainBinding binding;
+    GridView gridView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(R.layout.activity_default);
+
+        int[] images = {R.drawable.ic_bloc_mur,R.drawable.ic_bloc_mur,R.drawable.ic_bloc_mur,R.drawable.ic_bloc_mur,R.drawable.ic_bloc_mur,R.drawable.ic_bloc_mur,R.drawable.ic_bloc_mur,R.drawable.ic_bloc_mur,
+                R.drawable.ic_bloc_mur,R.drawable.ic_carre_blanc,R.drawable.ic_carre_blanc,R.drawable.ic_carre_blanc,R.drawable.ic_carre_blanc,R.drawable.ic_carre_blanc,R.drawable.ic_carre_blanc,R.drawable.ic_bloc_mur,
+                R.drawable.ic_bloc_mur,R.drawable.ic_carre_blanc,R.drawable.ic_bloc_mur,R.drawable.ic_carre_blanc,R.drawable.ic_bloc_mur,R.drawable.ic_carre_blanc,R.drawable.ic_carre_blanc,R.drawable.ic_bloc_mur,
+                R.drawable.ic_bloc_mur,R.drawable.ic_carre_blanc,R.drawable.ic_carre_blanc,R.drawable.ic_carre_blanc,R.drawable.ic_carre_blanc,R.drawable.ic_carre_blanc,R.drawable.ic_carre_blanc,R.drawable.ic_bloc_mur,
+                R.drawable.ic_bloc_mur,R.drawable.ic_carre_blanc,R.drawable.ic_bloc_mur,R.drawable.ic_bloc_mur,R.drawable.ic_carre_blanc,R.drawable.ic_carre_blanc,R.drawable.ic_carre_blanc,R.drawable.ic_bloc_mur,
+                R.drawable.ic_bloc_mur,R.drawable.ic_carre_blanc,R.drawable.ic_bloc_mur,R.drawable.ic_bloc_mur,R.drawable.ic_carre_blanc,R.drawable.ic_carre_blanc,R.drawable.ic_carre_blanc,R.drawable.ic_bloc_mur,
+                R.drawable.ic_bloc_mur,R.drawable.ic_carre_blanc,R.drawable.ic_carre_blanc,R.drawable.ic_carre_blanc,R.drawable.ic_carre_blanc,R.drawable.ic_carre_blanc,R.drawable.ic_carre_blanc,R.drawable.ic_bloc_mur,
+                R.drawable.ic_bloc_mur,R.drawable.ic_bloc_mur,R.drawable.ic_bloc_mur,R.drawable.ic_bloc_mur,R.drawable.ic_bloc_mur,R.drawable.ic_bloc_mur,R.drawable.ic_bloc_mur,R.drawable.ic_bloc_mur};
+
+        gridView=findViewById(R.id.gridView);
+
+        GridAdapter gridAdapter = new GridAdapter(Default.this,images);
+        gridView.setAdapter(gridAdapter);
     }
-
-    int[] images = {R.drawable.wall,R.drawable.wall,R.drawable.wall,R.drawable.wall,R.drawable.wall,R.drawable.wall,R.drawable.wall,R.drawable.wall,
-            R.drawable.wall,R.drawable.floor,R.drawable.floor,R.drawable.floor,R.drawable.floor,R.drawable.floor,R.drawable.floor,R.drawable.wall,
-            R.drawable.wall,R.drawable.floor,R.drawable.wall,R.drawable.floor,R.drawable.wall,R.drawable.floor,R.drawable.floor,R.drawable.wall,
-            R.drawable.wall,R.drawable.floor,R.drawable.floor,R.drawable.floor,R.drawable.floor,R.drawable.floor,R.drawable.floor,R.drawable.wall,
-            R.drawable.wall,R.drawable.floor,R.drawable.wall,R.drawable.wall,R.drawable.floor,R.drawable.floor,R.drawable.floor,R.drawable.wall,
-            R.drawable.wall,R.drawable.floor,R.drawable.wall,R.drawable.wall,R.drawable.floor,R.drawable.floor,R.drawable.floor,R.drawable.wall,
-            R.drawable.wall,R.drawable.floor,R.drawable.floor,R.drawable.floor,R.drawable.floor,R.drawable.floor,R.drawable.floor,R.drawable.wall,
-            R.drawable.wall,R.drawable.wall,R.drawable.wall,R.drawable.wall,R.drawable.wall,R.drawable.wall,R.drawable.wall,R.drawable.wall};
-
-    GridAdapter gridAdapter = new GridAdapter(MainActivity.this,images);
-        binding.gridView.setAdapter(gridAdapter);
 }
